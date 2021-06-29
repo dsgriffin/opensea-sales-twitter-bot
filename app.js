d16eb0cf3b9182ec00bf8919dd626c0ff2796831
Daniel Griffin <mail@dsgriffin.com>
2021-06-29 17:49:14 +0100

    Update with comments

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-// External
 const axios = require('axios');
 const _ = require('lodash');
 const moment = require('moment');
 const { ethers } = require('ethers');
-// Local
 const tweet = require('./tweet');
 
 function formatAndSendTweet(event) {
     const tokenName = _.get(event, ['asset', 'name']);
     const image = _.get(event, ['asset', 'image_url']);
     const ethPrice = _.get(event, 'total_price');
-
     const formattedPrice = ethers.utils.formatEther(ethPrice.toString());
+
+    // Don't tweet out sales below 0.5 (preference, can be changed)
+    if (Number(formattedPrice) < 0.5) {
+        console.log(`${tokenName} sold for ${formattedPrice}${ethers.constants.EtherSymbol}, below tweet price`);
+        return;
+    }
 
     const tweetText = `${tokenName} bought for ${formattedPrice}${ethers.constants.EtherSymbol}. #NFT #NFTs`;
 
-    return tweet.tweet(tweetText, image);
+    console.log(tweetText);
+
+    return tweet.handleDupesAndTweet(tweetText, image);
 }
 
+// Poll OpenSea every minute & retrieve all sales for a given collection in the last minute
+// Then pass those events over to the formatter before tweeting
 setInterval(() => {
     const lastMinute = moment().startOf('minute').subtract(59, "seconds").unix();
 
@@ -40,4 +47,3 @@ setInterval(() => {
         console.error(error);
     });
 }, 60000);
-
--- tweet.js
+++ tweet.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const twit = require('twit');
+const _ = require('lodash');
 
 const twitterConfig = {
     consumer_key: process.env.CONSUMER_KEY,
@@ -10,12 +11,42 @@ const twitterConfig = {
 
 const twitterClient = new twit(twitterConfig);
 
+// OpenSea doesn't give us access to Webhooks; need to poll every 60 seconds
+// Occasionaly in the split second of delay, dupelicates are retrieved - filter them out here
+async function handleDupesAndTweet(tweetText, imageUrl) {
+    // Search our twitter account's recent tweets for anything exactly matching our new tweet's text
+    twitterClient.get('search/tweets', { q: tweetText, count: 1, result_type: 'recent' }, (err, data, response) => {
+        if (!error) {
+            const statuses = _.get(data, 'statuses');
+
+            console.log(data);
+
+            // No duplicate statuses found
+            if (_.isEmpty(data) || _.isEmpty(statuses)) {
+                return tweet(tweetText, imageUrl);
+            }
+
+            const statusOlderThan15Mins = moment(_.get(statuses[0], 'created_at')).isBefore(moment().subtract(15, 'minutes'));
+
+            // Status found is older than 15 minutes, not a cached transaction
+            if (statusOlderThan15Mins) {
+                return tweet(tweetText, imageUrl);
+            }
+
+            console.error('Tweet is a duplicate; possible old transaction retrieved from OpenSea');
+        } else {
+            console.error(err);
+        }
+    });
+}
+
+// Upload image of item retrieved from OpenSea & then tweet that image + provided text
 async function tweet(tweetText, imageUrl) {
+    // Format our image to base64
     const processedImage = await getBase64(imageUrl);
 
+    // Upload the item's image from OpenSea to Twitter & retrieve a reference to it
     twitterClient.post('media/upload', { media_data: processedImage }, (error, media, response) => {
-        // Image has now been uploaded to Twitter
-        // Post tweet containing this image + provided text
         if (!error) {
             const tweet = {
                 status: tweetText,
@@ -35,10 +66,11 @@ async function tweet(tweetText, imageUrl) {
     });
 }
 
+// Format a provided URL into it's base64 representation
 function getBase64(url) {
     return axios.get(url, { responseType: 'arraybuffer'}).then(response => Buffer.from(response.data, 'binary').toString('base64'))
 }
 
 module.exports = {
-    tweet: tweet,
+    handleDupesAndTweet: handleDupesAndTweet
 };
