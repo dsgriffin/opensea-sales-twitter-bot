fea80daf522bd41b9665b136fd6e32e72cd66ccf
Daniel Griffin <mail@dsgriffin.com>
2021-06-29 18:23:29 +0100

    Add moment

--- tweet.js
+++ tweet.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const twit = require('twit');
+const moment = require('moment');
 const _ = require('lodash');
 
 const twitterConfig = {
@@ -15,25 +16,24 @@ const twitterClient = new twit(twitterConfig);
 // Occasionaly in the split second of delay, dupelicates are retrieved - filter them out here
 async function handleDupesAndTweet(tweetText, imageUrl) {
     // Search our twitter account's recent tweets for anything exactly matching our new tweet's text
-    twitterClient.get('search/tweets', { q: tweetText, count: 1, result_type: 'recent' }, (err, data, response) => {
+    twitterClient.get('search/tweets', { q: tweetText, count: 1, result_type: 'recent' }, (error, data, response) => {
         if (!error) {
             const statuses = _.get(data, 'statuses');
-
-            console.log(data);
 
             // No duplicate statuses found
             if (_.isEmpty(data) || _.isEmpty(statuses)) {
                 return tweet(tweetText, imageUrl);
             }
 
-            const statusOlderThan15Mins = moment(_.get(statuses[0], 'created_at')).isBefore(moment().subtract(15, 'minutes'));
+            const mostRecentMatchingTweetCreatedAt = _.get(statuses[0], 'created_at');
+            const statusOlderThan10Mins = moment(mostRecentMatchingTweetCreatedAt).isBefore(moment().subtract(10, 'minutes'));
 
-            // Status found is older than 15 minutes, not a cached transaction
-            if (statusOlderThan15Mins) {
+            // Status found is older than 10 minutes, not a cached transaction, just sold at same price
+            if (statusOlderThan10Mins) {
                 return tweet(tweetText, imageUrl);
             }
 
-            console.error('Tweet is a duplicate; possible old transaction retrieved from OpenSea');
+            console.error('Tweet is a duplicate; possible delayed transaction retrieved from OpenSea');
         } else {
             console.error(err);
         }
