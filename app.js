1bc3add5a1ea4bdfaa6e292559102568c7c8e0cd
Daniel Griffin <mail@dsgriffin.com>
2021-08-31 18:46:46 +0100

    update

--- app.js
+++ app.js
@@ -16,32 +16,32 @@ function formatAndSendTweet(event) {
     const tokenSymbol = _.get(event, ['payment_token', 'symbol']);
 
     // OPTIONAL - if you want to tweet a status including the image too
-    const imageUrl = _.get(event, ['asset', 'image_url']);
+    // const imageUrl = _.get(event, ['asset', 'image_url']);
 
     const isEthSale = (tokenSymbol === 'WETH' || tokenSymbol === 'ETH');
     const formattedEthPrice = ethers.utils.formatEther(totalPrice.toString());
     const formattedUsdPrice = (formattedEthPrice * usdValue).toFixed(2);
 
     // OPTIONAL - don't tweet out sales below 1 ETH (preference, can be changed)
-    // if (Number(formattedEthPrice) < 1.5) {
-    //     console.log(`${tokenName} sold for ${formattedEthPrice}${ethers.constants.EtherSymbol}, below tweet price`);
-    //     return;
-    // }
+    if (Number(formattedEthPrice) < 2) {
+        console.log(`${tokenName} sold for ${formattedEthPrice}${ethers.constants.EtherSymbol}, below tweet price`);
+        return;
+    }
 
     let tweetText;
 
     if (isEthSale) {
-        tweetText = `${tokenName} bought for ${formattedEthPrice}Ξ ($${formattedUsdPrice}) #TheCreation #NFT ${openseaLink}`;
+        tweetText = `${tokenName} bought for ${formattedEthPrice}Ξ ($${formattedUsdPrice}) #EmblemVault $COVAL ${openseaLink}`;
     } else {
-        tweetText = `${tokenName} bought for ${formattedUsdPrice} ${tokenSymbol} #TheCreation #NFT ${openseaLink}`;
+        tweetText = `${tokenName} bought for ${formattedUsdPrice} ${tokenSymbol} #EmblemVault $COVAL ${openseaLink}`;
     }
 
     console.log(tweetText);
 
     // OPTIONAL - if you want the tweet to include an attached image
-    return tweet.tweetWithImage(tweetText, imageUrl);
+    // return tweet.tweetWithImage(tweetText, imageUrl);
 
-    // return tweet.tweet(tweetText);
+    return tweet.tweet(tweetText);
 }
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
