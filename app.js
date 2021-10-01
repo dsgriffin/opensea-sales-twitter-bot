07dcc9193df7c87f3b7fa4a9701d0dc5c22a23b6
MF POOMA <nick@mfproductions.biz>
2021-10-01 12:33:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function formatAndSendTweet(event) {
     const formattedEthPrice = formattedUnits * tokenEthPrice;
     const formattedUsdPrice = formattedUnits * tokenUsdPrice;
 
-    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT ${openseaLink}`;
+    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #DesignerPunks #NFT #Polygon ${openseaLink}`;
 
     console.log(tweetText);
 
