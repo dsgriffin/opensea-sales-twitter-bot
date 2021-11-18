933cd20a54f77c17e189cd5262b85c16003137f1
MF POOMA <nick@mfproductions.biz>
2021-11-18 16:43:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function formatAndSendTweet(event) {
     const formattedEthPrice = formattedUnits * tokenEthPrice;
     const formattedUsdPrice = formattedUnits * tokenUsdPrice;
 
-    const tweetText = `POOMA LOOMPA DOOMPADEE DOO,
+    const tweetText = `POOMPA LOOMPA DOOMPADEE DOO,
     I'VE GOT ANOTHER SALE FOR YOU!
     
     ${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #MFPOOMA #MFP #MFWONKA #NFT ${openseaLink}`;
